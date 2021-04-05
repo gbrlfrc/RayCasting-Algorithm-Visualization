a16d82cf2e05db7637bc8367b2ed57d0f9f0edbb
gabriele fraracci <gbrl.frc@protonmail.ch>
2021-04-05 14:16:58 +0000

    random shape

--- src/boundaryShape.js
+++ src/boundaryShape.js
@@ -1,4 +1,4 @@
-const getShape = (num) => {
+const getShape = (opt) => {
     const oneBound  = [{
         pt1 : { x : (windowDim.width/10)*3, y : windowDim.height/2},
         pt2 : { x : (windowDim.width/10)*7, y : windowDim.height/2}   
@@ -63,10 +63,49 @@ const getShape = (num) => {
         pt1 : { x : (windowDim.width/10)*3, y : (windowDim.height/10)*8},
         pt2 : { x : (windowDim.width/10)*7, y : (windowDim.height/10)*8}  
     }];
+    const rect = [{
+        pt1 : { x : (windowDim.width/10)*6, y : (windowDim.height/10)*3},
+        pt2 : { x : (windowDim.width/10)*6, y : (windowDim.height/10)*7}  
+    },
+    {
+        pt1 : { x : (windowDim.width/10)*6, y : (windowDim.height/10)*3},
+        pt2 : { x : (windowDim.width/10)*7, y : (windowDim.height/10)*3}  
+    }, 
+    {
+        pt1 : { x : (windowDim.width/10)*7, y : (windowDim.height/10)*3},
+        pt2 : { x : (windowDim.width/10)*7, y : (windowDim.height/10)*7}  
+    }, 
+    {
+        pt1 : { x : (windowDim.width/10)*6, y : (windowDim.height/10)*7},
+        pt2 : { x : (windowDim.width/10)*7, y : (windowDim.height/10)*7}  
+    }];
+    const angle=[{
+        pt1 : { x : (windowDim.width/10)*6, y : (windowDim.height/10)*3},
+        pt2 : { x : (windowDim.width/10)*6, y : (windowDim.height/10)*7}  
+    },
+    {
+        pt1 : { x : (windowDim.width/10)*6, y : (windowDim.height/10)*3},
+        pt2 : { x : (windowDim.width/10)*7, y : (windowDim.height/10)*3}  
+    }];
 
-    if (num == 1) return oneBound;
-    else if (num == 2) return twoBound;
-    else if (num == 3) return threeBound;
-    else if (num == 4) return fourBound;
-    else return fiveBound;
+    if (opt.shape === 'boundary'){
+        if (opt.bound == 1) return oneBound;
+        else if (opt.bound == 2) return twoBound;
+        else if (opt.bound == 3) return threeBound;
+        else if (opt.bound == 4) return fourBound;
+        else return fiveBound;    
+    }else if (opt.shape === 'rectangle'){
+        return rect;
+    }else if (opt.shape === 'random'){
+        const r = [];
+        for (let i=0; i< opt.bound; i++){
+            r.push({
+                pt1:{x: random(window.innerWidth), y: random(window.innerHeight)},
+                pt2:{x: random(window.innerWidth), y: random(window.innerHeight)}
+            })            
+        }
+        return r;
+    }else {
+        return angle;
+    }
 }
--- src/index.js
+++ src/index.js
@@ -5,20 +5,40 @@ const windowDim = {
 let ray;
 let walls = [];
 let boundary = [];
+const opt= {
+    shape : 'boundary',
+    bound : 2 
+}
+const offset= {x:0.0,y:0.0}
 
 const update = () => {
+
     while (walls.length>0){
         walls.pop();
         boundary.pop();
+    }
+    for (let el of document.getElementsByName('shape')){
+        if (el.checked){
+            if (el.value !== 'boundary' && el.value !== 'random'){
+                document.getElementById('slider').disabled = true;
+                opt.shape = el.value;
+                opt.bound = null;
+            }else {
+                document.getElementById('slider').disabled = false;
+                opt.shape = el.value;
+                opt.bound = document.getElementById('slider').value;
+            }
+        }
     }
     document.getElementById('number').textContent = document.getElementById('slider').value;
     setup();
 }
 
 function setup() {
+
     createCanvas(windowDim.width, windowDim.height);
 
-    const shape = getShape(document.getElementById('slider').value);
+    const shape = getShape(opt);
     for (let boundCoord of shape){
         walls.push(boundCoord);
         boundary.push(new Boundary(boundCoord.pt1, boundCoord.pt2));
@@ -32,10 +52,19 @@ function setup() {
 function draw() {
     background(0);
     stroke(255);
+    ////////////////////////////////////////// perline noise movement
+    // offset.x+=0.01;
+    // offset.y+=0.01;
+    // const rayCoord = {
+    //     x : noise(offset.x)*windowDim.width,
+    //     y : noise(offset.y)*windowDim.height,
+    // }
+    //////////////////////////////////////// mouse movement
     const rayCoord = {
         x : mouseX,
         y : mouseY,
     }
+    ///////////////////////////////////////
     ray = new Ray(rayCoord);
     for (let bound of boundary){bound.show();}
     ray.look(walls);
